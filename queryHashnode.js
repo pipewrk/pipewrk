7615c21f94eb59bcbb2ef4544c76cade892c4dc1
Jason Nathan <jjnathanjr@gmail.com>
2024-06-22 03:20:40 +0800

    Create queryHashnode.js

---
+++ queryHashnode.js
@@ -0,0 +1,31 @@
+const fetch = require('node-fetch');
+
+async function getPostDetails(postId) {
+  const query = `
+    query GetPostDetails($postId: String!) {
+      post(id: $postId) {
+        title
+        brief
+        slug
+        author {
+          username
+        }
+      }
+    }
+  `;
+
+  const variables = { postId };
+
+  const response = await fetch('https://api.hashnode.com/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ query, variables }),
+  });
+
+  const result = await response.json();
+  return result.data.post;
+}
+
+module.exports = { getPostDetails };
