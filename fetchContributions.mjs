c6cdf8bbefac8705c294496c7a4e7851702ae0e6
Jason Nathan <jjnathanjr@gmail.com>
2024-06-24 08:44:26 +0800

    Update fetchContributions.mjs

--- fetchContributions.mjs
+++ fetchContributions.mjs
@@ -1,4 +1,5 @@
 import { graphql } from '@octokit/graphql';
+import fetch from 'node-fetch';
 
 export async function fetchContributions(username, token) {
   const query = `
@@ -25,12 +26,29 @@ export async function fetchContributions(username, token) {
     }
   `;
 
-  const response = await graphql(query, {
-    username,
-    headers: {
-      authorization: `token ${token}`,
-    },
-  });
+  try {
+    const octokitGraphQL = graphql.defaults({
+      headers: {
+        authorization: `token ${token}`,
+      },
+      request: {
+        fetch
+      }
+    });
 
-  return response.user.contributionsCollection.commitContributionsByRepository;
+    const response = await octokitGraphQL(query, {
+      username
+    });
+
+    // Check if the response includes the expected data
+    if (!response || !response.user || !response.user.contributionsCollection) {
+      throw new Error('Invalid response structure received from GitHub API.');
+    }
+
+    return response.user.contributionsCollection.commitContributionsByRepository;
+  } catch (error) {
+    console.error(`Failed to fetch contributions for user ${username}: ${error.message}`);
+    // Re-throw the error to be caught by the workflow or further error handling logic
+    throw error;
+  }
 }
