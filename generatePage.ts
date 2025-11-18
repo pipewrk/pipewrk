b06e0aa875678e901139469330a07c2f67680ff9
Jason Joseph Nathan <780157+pipewrk@users.noreply.github.com>
2025-11-18 18:38:23 +0800

    Change page title from 'Jason Nathan' to 'pipewrk'

--- generatePage.ts
+++ generatePage.ts
@@ -28,7 +28,7 @@ const htmlTemplate = `<!DOCTYPE html>
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Jason Nathan</title>
+  <title>pipewrk</title>
 
   <style>
     @import url('https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,100..900;1,100..900&family=Ysabeau+Office:ital,wght@0,100..900;1,100..900&display=swap');
@@ -117,7 +117,6 @@ const htmlTemplate = `<!DOCTYPE html>
 
   <!-- JSON-LD Schemas -->
   <script type="application/ld+json" src="/nginx-by-example.schema.json"></script>
-  <script type="application/ld+json" src="/jason-profile.schema.json"></script>
 </head>
 <body>
   <main>
